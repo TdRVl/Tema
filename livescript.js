a4a034260aa407e78abab46b04f22c13f3c30995
Valean Tudor <valean.tudor2004@gmail.com>
2025-03-31 16:10:53 +0300

    make search in wik

--- livescript.js
+++ livescript.js
@@ -5,31 +5,35 @@ async function fetchCountries() {
         name: country.name.common,
         flag: country.flags.svg,
         population: country.population.toLocaleString(),
-        capital: country.capital ? country.capital[0] : 'No capital' 
+        capital: country.capital ? country.capital[0] : 'No capital'
     }));
 }
 
 async function searchCountries() {
     const query = document.getElementById('search').value.toLowerCase();
     const resultsContainer = document.getElementById('results');
-    resultsContainer.innerHTML = ''; 
+    resultsContainer.innerHTML = '';
 
-    if (!query) return; 
-    const countries = await fetchCountries(); 
-    const filtered = countries.filter(c => c.name.toLowerCase().startsWith(query)); 
+    if (!query) return;
+    const countries = await fetchCountries();
+    const filtered = countries.filter(c => c.name.toLowerCase().startsWith(query));
 
     if (filtered.length === 0) {
         resultsContainer.innerHTML = '<p>No countries found.</p>';
     } else {
         filtered.forEach(c => {
-            resultsContainer.innerHTML += `
-                <div class="country">
-                    <span>${c.name}</span>
-                    <span>${c.population} people</span>
-                    <span>Capital: ${c.capital}</span> <!-- Show capital city -->
-                    <img class="flag" src="${c.flag}" alt="Flag of ${c.name}">
-                </div>
+            const countryDiv = document.createElement('div');
+            countryDiv.classList.add('country');
+            countryDiv.innerHTML = `
+                <span>${c.name}</span>
+                <span>${c.population} people</span>
+                <span>Capital: ${c.capital}</span>
+                <img class="flag" src="${c.flag}" alt="Flag of ${c.name}">
             `;
+            countryDiv.addEventListener('click', () => {
+                window.open(`https://en.wikipedia.org/wiki/${c.name}`, '_blank');
+            });
+            resultsContainer.appendChild(countryDiv);
         });
     }
 }
